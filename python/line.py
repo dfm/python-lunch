2902ac150a93b9b169856eba7e77818a3e97adcb
Dan F-M <danfm@nyu.edu>
2012-07-19 23:47:49 +0200

    removing stuff to get skeleton

--- python/line.py
+++ python/line.py
@@ -1,10 +1,5 @@
 __all__ = ["lnlike"]
 
 
-import numpy as np
-
-
 def lnlike(p, data):
-    m, b = p
-    chi2 = np.sum(((data[:, 1] - (m * data[:, 0] + b)) / data[:, 2]) ** 2)
-    return -0.5 * chi2
+    raise NotImplementedError()
